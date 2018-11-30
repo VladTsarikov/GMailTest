753e8e41d0f5337f8847ded0b5219b4693e77bff
Vlad Tsarikov <tsarikov1995@gmail.com>
2018-11-30 15:07:19 +0300

    Add waitings

--- src/test/java/framework/webdriver/waitings/Condition.java
+++ src/test/java/framework/webdriver/waitings/Condition.java
@@ -59,7 +59,7 @@ class Condition {
             }
         };
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
